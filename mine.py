b446a95e00869438e2c2dbd89d12baf636a60690
Novellina <nataliedenisova@gmail.com>
2024-11-16 21:14:35 +0500

    hi
    hi

--- mine.py
+++ mine.py
@@ -1,1 +1,10 @@
-print ('hello')
+#print ('hello')
+#is_raining = True
+#is_sunny = not is_raining
+#print (is_sunny)
+
+first = input()
+second = input()
+third = input()
+name = int (input ('Введите три целых числа:', (first, second, third)))
+print (name)
